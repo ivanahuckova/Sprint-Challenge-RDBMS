8f1ef957c08bc162c4dee23afa9b860fa6b04b51
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-29 09:46:36 +0100

    Seed the database

--- data/seeds/01-actions.js
+++ data/seeds/01-actions.js
@@ -1,13 +1,16 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex('actions')
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('actions').insert([
+        { action_id: 1, action_description: 'Description of action nr. 1', action_notes: 'Notes about action 1', completed: false },
+        { action_id: 2, action_description: 'Description of action nr. 2', action_notes: 'Notes about action 2', completed: false },
+        { action_id: 3, action_description: 'Description of action nr. 3', action_notes: 'Notes about action 3', completed: false },
+        { action_id: 4, action_description: 'Description of action nr. 4', action_notes: 'Notes about action 4', completed: false },
+        { action_id: 5, action_description: 'Description of action nr. 5', action_notes: 'Notes about action 5', completed: false },
+        { action_id: 6, action_description: 'Description of action nr. 6', action_notes: 'Notes about action 6', completed: false }
       ]);
     });
 };
--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -5,12 +5,12 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('projects').insert([
-        { id: 1, project_name: 'Project 1', project_description: 'Create project with id 1', completed: false },
-        { id: 2, project_name: 'Project 2', project_description: 'Create project with id 2', completed: false },
-        { id: 3, project_name: 'Project 3', project_description: 'Create project with id 3', completed: false },
-        { id: 4, project_name: 'Project 4', project_description: 'Create project with id 4', completed: false },
-        { id: 5, project_name: 'Project 5', project_description: 'Create project with id 5', completed: false },
-        { id: 6, project_name: 'Project 6', project_description: 'Create project with id 6', completed: false }
+        { project_id: 1, project_name: 'Project 1', project_description: 'Create project with id 1', completed: false },
+        { project_id: 2, project_name: 'Project 2', project_description: 'Create project with id 2', completed: false },
+        { project_id: 3, project_name: 'Project 3', project_description: 'Create project with id 3', completed: false },
+        { project_id: 4, project_name: 'Project 4', project_description: 'Create project with id 4', completed: false },
+        { project_id: 5, project_name: 'Project 5', project_description: 'Create project with id 5', completed: false },
+        { project_id: 6, project_name: 'Project 6', project_description: 'Create project with id 6', completed: false }
       ]);
     });
 };
